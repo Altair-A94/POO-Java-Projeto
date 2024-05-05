9969feb05aa41b9fcc71aec3f288e23223d78fbc
guguivan <164552691+guguivan@users.noreply.github.com>
2024-05-05 13:49:53 -0300

    Inicio cod Projeto

---
+++ Produto.java
@@ -0,0 +1,45 @@
+
+package projeto_jdbc;
+
+public class Produto {
+ 
+    private int id;
+    private String nome;
+    private double preco;
+    
+    // Construtor
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+    
+   
+   // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+}
+
+
---
+++ Projeto_Jdbc.java
@@ -0,0 +1,12 @@
+
+package projeto_jdbc;
+
+
+public class Projeto_Jdbc {
+
+    public static void main(String[] args) {
+       
+       
+    }
+    
+}
