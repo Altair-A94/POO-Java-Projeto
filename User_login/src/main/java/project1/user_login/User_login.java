17047729959900125d0dddc99b5d3659574e0d72
guguivan <164552691+guguivan@users.noreply.github.com>
2024-06-07 11:50:45 -0300

    Add files via upload

---
+++ User_login/src/main/java/project1/user_login/User_login.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project1.user_login;
+
+/**
+ *
+ * @author augus
+ */
+public class User_login {
+
+    public static void main(String[] args) {
+        Form1 f1 = new Form1();
+        f1.setVisible(true);
+        
+        
+    }
+}
